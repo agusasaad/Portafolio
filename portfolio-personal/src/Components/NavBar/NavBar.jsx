bfa49ff84ffa7efec81bb5ac37e8cf0a05392196
agusasaad <agusasaad1099@hotmail.com>
2024-01-31 10:40:14 -0300

    first commit

---
+++ portfolio-personal/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <p className="logo_portfolio">AA</p>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
